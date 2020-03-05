019bfdcc3711ec31e4c5ecbac0316736e3229b78
Heather Hennessey <heatherhennessey42@gmail.com>
2020-03-05 11:01:07 -0500

    Keyboard and text input spawn on click. Text updating with user input in progress.

--- src/worlds/hub/scripts/generator.js
+++ src/worlds/hub/scripts/generator.js
@@ -1,25 +1,36 @@
 AFRAME.registerComponent('generator', {
     schema: {  
-        message_number: {type: 'number', default: 0}
+        message_number: {type: 'number', default: 0},
+        message_position: {type: 'number', default: 0.5},
             
     },
     init: function() {
         const Context_AF = this;
-       
+
         Context_AF.el.addEventListener('click', (e) => {
             console.log("Generator clicked");
 
-            createNewMessage();
+            this.createNewMessage();
            
         });
     },
 
     createNewMessage: function(){
-        newMessage = document.createElement('a-entity');
-        newMessage.setAttribute('id', "msg" + String(Context_AF.message_number));
-        newMessage.setAttribute('message');
-        newMessage.setAttribute('geometry', {primitive: box});
-        newMessage.setAttribute('position', "4.2 0 1.5");
+        const Context_AF = this;
+        let yPos = Context_AF.message_position + (Context_AF.message_number * .5);
+        const container = document.querySelector('a-scene');
+
+        console.log("Creating new message");
+        Context_AF.newMessage = document.createElement('a-entity');
+        Context_AF.newMessage.setAttribute('id', "msg"); // + String(Context_AF.message_number)
+        Context_AF.newMessage.setAttribute('class', "interactive");
+        Context_AF.newMessage.setAttribute('message', '');
+        Context_AF.newMessage.setAttribute('circles-interactive-object', '');
+        Context_AF.newMessage.setAttribute('geometry', {primitive: "box", width: 0.2, depth: 0.05, height: 0.1});
+        Context_AF.newMessage.setAttribute('position', {x: -1, y:1.17, z:4.36});
+        Context_AF.newMessage.setAttribute("material", {color: '#fff'});
+        console.log(Context_AF.newMessage);
+        container.appendChild(Context_AF.newMessage);
 
 
         Context_AF.message_number = Context_AF.message_number += 1;
--- src/worlds/hub/scripts/message.js
+++ src/worlds/hub/scripts/message.js
@@ -5,11 +5,61 @@ AFRAME.registerComponent('message', {
     },
     init: function() {
         const Context_AF = this;
+        Context_AF.el.setAttribute('circles-interactive-object');
 
         Context_AF.el.addEventListener('click', (e) => {
-            console.log(message_text);
-            console.log(this.data.message_text);
+            this.createNewText();
         });
+    },
+
+    createNewText: function(){
+        const Context_AF = this;
+        const container = document.querySelector('a-scene');
+        Context_AF.textInput = document.querySelector('#input');
+        // Context_AF.keyboardEl = document.createElement("a-entity");
+        // Context_AF.keyboardEl.setAttribute("id", 'keyboard');
+        // Context_AF.keyboardEl.setAttribute('position', {x: -1, y:1.17, z:4.0});
+        // Context_AF.keyboardEl.setAttribute("components", 'keyboard');
+        // Context_AF.keyboardEl.setAttribute('scale', '1 1 1');
+        // Context_AF.keyboardEl.setAttribute("rotation", '0 180 0');
+        // container.appendChild(Context_AF.keyboardEl);
+        
+        // Context_AF.textInput = document.createElement("a-text");
+        // Context_AF.textInput.setAttribute("id", 'input');
+        // Context_AF.textInput.setAttribute('position', {x: -1, y:1.25, z:4});
+        // Context_AF.textInput.setAttribute('color', '#fff');
+        // Context_AF.textInput.setAttribute('scale', '0.5 0.5 0.5');
+        // Context_AF.textInput.setAttribute('rotation', '0 180 0');
+        // container.appendChild(Context_AF.textInput);
+
+        Context_AF.textInput.addEventListener('click', (e) => {
+            console.log("text clicked");
+            Context_AF.updateText(e);
+        });
+    },
+
+    updateText: function(e){
+        const Context_AF = this;
+        
+        var code = parseInt(e.detail.code)
+        switch(code) {
+          case 8:
+            Context_AF.message_text = Context_AF.message_text.slice(0, -1)
+            break
+          case 06:
+            alert('submitted')
+            
+            document.querySelector('#input').setAttribute('value', Context_AF.message_text)
+            document.querySelector('#input').setAttribute('color', 'blue')
+            Context_AF.keyboardEl.parentNode.removeChild(keyboard)
+            return
+          default:
+            Context_AF.message_text = Context_AF.message_text + e.detail.value
+            break
+          }
+        document.querySelector('#input').setAttribute('value', Context_AF.message_text + '_')
+      
+
     },
 
 }); 
