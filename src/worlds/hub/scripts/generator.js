857df4daf97b41a126d56dbca0494eecc7a79e7c
Heather Hennessey <heatherhennessey42@gmail.com>
2020-03-08 17:21:09 -0400

    Fridge magnet words IP for changing message input

--- src/worlds/hub/scripts/generator.js
+++ src/worlds/hub/scripts/generator.js
@@ -26,7 +26,7 @@ AFRAME.registerComponent('generator', {
         Context_AF.newMessage.setAttribute('class', "interactive");
         Context_AF.newMessage.setAttribute('message', '');
         Context_AF.newMessage.setAttribute('circles-interactive-object', '');
-        Context_AF.newMessage.setAttribute('circles-inspect-object', 'inspectScale:0.5 0.5 0.5; textRotationY:0.0; textLookAt:false;;');
+        Context_AF.newMessage.setAttribute('circles-inspect-object', 'inspectScale:1 1 1; textRotationY:0.0; textLookAt:false;;');
         Context_AF.newMessage.setAttribute('geometry', {primitive: "box", width: 0.2, depth: 0.05, height: 0.1});
         Context_AF.newMessage.setAttribute('position', {x: -1, y:1.17, z:4.36});
         Context_AF.newMessage.setAttribute("material", {color: '#fff'});
@@ -36,6 +36,12 @@ AFRAME.registerComponent('generator', {
         //console.log(Context_AF.newMessage);
         scene.appendChild(Context_AF.newMessage);
 
+        Context_AF.messageText = document.createElement('a-text');
+        Context_AF.messageText.setAttribute('value', "Message");
+        Context_AF.messageText.setAttribute('id', "input");
+       // Context_AF.messageText.setAttribute('width', "input");
+        Context_AF.newMessage.appendChild(Context_AF.messageText);
+ 
 
         Context_AF.message_number = Context_AF.message_number += 1;
     },
--- src/worlds/hub/scripts/message.js
+++ src/worlds/hub/scripts/message.js
@@ -15,53 +15,58 @@ AFRAME.registerComponent('message', {
     createNewText: function(){
         const Context_AF = this;
         const msg = document.querySelector('#msg');
-        //Context_AF.textInput = document.querySelector('#input');
+        
+       
         Context_AF.newMessage = document.createElement('a-text');
         Context_AF.newMessage.setAttribute('value', "Boo");
         Context_AF.newMessage.setAttribute('id', "msgBoo");
-        Context_AF.newMessage.setAttribute('position', {x: -1, y:0.2, z:0});
+        Context_AF.newMessage.setAttribute('position', {x: -1, y:0.5, z:0});
+        Context_AF.newMessage.setAttribute('circles-interactive-object', '');
+        Context_AF.newMessage.setAttribute('geometry', {primitive: 'plane'});
 
         Context_AF.newMessage1 = document.createElement('a-text');
         Context_AF.newMessage1.setAttribute('value', "Foo");
         Context_AF.newMessage1.setAttribute('id', "msgFoo");
-        Context_AF.newMessage1.setAttribute('position', {x: 1, y:0.2, z:0});
+        Context_AF.newMessage1.setAttribute('position', {x: 1, y:0.5, z:0});
+        Context_AF.newMessage1.setAttribute('circles-interactive-object', '');
+        Context_AF.newMessage1.setAttribute('geometry', {primitive: 'plane'});
+
 
         Context_AF.newMessage2 = document.createElement('a-text');
         Context_AF.newMessage2.setAttribute('value', "Baz");
         Context_AF.newMessage2.setAttribute('id', "msgBaz");
-        Context_AF.newMessage2.setAttribute('position', {x: 0, y:0.2, z:0});
+        Context_AF.newMessage2.setAttribute('position', {x: 0, y:0.5, z:0});
+        Context_AF.newMessage2.setAttribute('circles-interactive-object', '');
+        Context_AF.newMessage2.setAttribute('geometry', {primitive: 'plane'});
+
 
         
         msg.appendChild(Context_AF.newMessage);
         msg.appendChild(Context_AF.newMessage1);
         msg.appendChild(Context_AF.newMessage2);
-        
-        Context_AF.textInput.addEventListener('click', (e) => {
+
+        Context_AF.newMessage.addEventListener('click', (e) => {
             console.log("text clicked" + e.detail);
+            Context_AF.updateText(e);
+        });
+        Context_AF.newMessage1.addEventListener('click', (e) => {
+            console.log("text 1 clicked" + e.detail);
+            Context_AF.updateText(e);
+        });
+
+        Context_AF.newMessage2.addEventListener('click', (e) => {
+            console.log("text 2 clicked" + e.detail);
             Context_AF.updateText(e);
         });
     },
 
     updateText: function(e){
         const Context_AF = this;
-        
-        var code = parseInt(e.detail.code)
-        switch(code) {
-          case 8:
-            Context_AF.message_text = Context_AF.message_text.slice(0, -1)
-            break
-          case 06:
-            alert('submitted')
-            
-            document.querySelector('#input').setAttribute('value', Context_AF.message_text)
-            document.querySelector('#input').setAttribute('color', 'blue')
-            Context_AF.keyboardEl.parentNode.removeChild(keyboard)
-            return
-          default:
-            Context_AF.message_text = Context_AF.message_text + e.detail.value
-            break
-          }
-        document.querySelector('#input').setAttribute('value', Context_AF.message_text + '_')
+        Context_AF.textInput = document.querySelector('#input');
+        let currentInput = Context_AF.textInput.getAttribute('value');
+       console.log('made it');
+       // Context_AF.textInput.setAttribute('value', currentInput + e.detail.)
+        //document.querySelector('#input').setAttribute('value', Context_AF.message_text + '_')
       
 
     },
