376ac6e52bff6a805a50c0737d41aaf1058f9f78
ascavarelli <anthony.scavarelli@gmail.com>
2020-06-26 20:45:44 -0400

    cleaning up and removing refs to opacity changes

--- src/components/circles-inspect-object.js
+++ src/components/circles-inspect-object.js
@@ -15,11 +15,6 @@ AFRAME.registerComponent('circles-inspect-object', {
   init: function() {
     const Context_AF = this;
     const data = this.data;
-    Context_AF.highlight_object = null;
-
-    Context_AF.el.addEventListener(CIRCLES.EVENTS.OBJECT_HIGHLIGHT_LOADED, (e) => {
-        Context_AF.highlight_object = e.target;
-    });
 
     if (data.origPos.x > 10000) {
       Context_AF.el.setAttribute('circles-inspect-object', {origPos:{x:Context_AF.el.object3D.position.x, y:Context_AF.el.object3D.position.y, z:Context_AF.el.object3D.position.z}}); //save it so network syncs this
@@ -35,14 +30,6 @@ AFRAME.registerComponent('circles-inspect-object', {
 
     let eventsAttached = false;
     Context_AF.el.addEventListener(CIRCLES.EVENTS.OBJECT_NETWORKED_ATTACHED, function (event) {
-      // console.log('WHWHWHWHWHWHWHHW');
-      // console.log("this");
-      // console.log(Context_AF.el.components);
-      // console.log(Context_AF.el.components['networked']);
-      // console.log(Context_AF.el.components['circles-inspect-object']);
-      // console.log( 'networked' in Context_AF.el.components );
-      // console.log( Context_AF.el.components.hasOwnProperty('networked') );
-
       if (eventsAttached === false) {
         eventsAttached = true;
         NAF.utils.getNetworkedEntity(Context_AF.el).then((el) => {
@@ -55,7 +42,6 @@ AFRAME.registerComponent('circles-inspect-object', {
           });
   
           el.addEventListener('ownership-lost', (e) => {
-            //Context_AF.el.removeAttribute('circles-parent-constraint');
             console.log("ownership-lost");
             Context_AF.el.emit( CIRCLES.EVENTS.OBJECT_OWNERSHIP_LOST, Context_AF.el, true );
           });
@@ -73,21 +59,15 @@ AFRAME.registerComponent('circles-inspect-object', {
         eventsAttached = true;
         NAF.utils.getNetworkedEntity(Context_AF.el).then((el) => {
 
-          //console.log("adding events");
-  
           el.addEventListener('ownership-gained', (e) => {
-            //console.log("ownership-gained");
             Context_AF.el.emit( CIRCLES.EVENTS.OBJECT_OWNERSHIP_GAINED, Context_AF.el, true );
           });
   
           el.addEventListener('ownership-lost', (e) => {
-            //Context_AF.el.removeAttribute('circles-parent-constraint');
-            //console.log("ownership-lost");
             Context_AF.el.emit( CIRCLES.EVENTS.OBJECT_OWNERSHIP_LOST, Context_AF.el, true );
           });
   
           el.addEventListener('ownership-changed', (e) => {
-            //console.log("ownership-changed");
             Context_AF.el.emit( CIRCLES.EVENTS.OBJECT_OWNERSHIP_CHANGED, Context_AF.el, true );
           });
         });
@@ -97,25 +77,7 @@ AFRAME.registerComponent('circles-inspect-object', {
 
     //send click event to manager
     Context_AF.el.addEventListener('click', (e) => {
-        //Context_AF.toggleInspection(evt);
-        // const eventObject = {
-        //                       inspectObject:Context_AF.el,
-        //                       inspectHighlight:Context_AF.highlight_object, 
-        //                       title:data.title, 
-        //                       description:data.description,
-        //                       inspectScale:data.inspectScale,
-        //                       inspectRotation:data.inspectRotation,
-        //                       textRotationY:data.textRotationY,
-        //                       textLookAt:data.textLookAt,
-        //                       origPos:Context_AF.origPos,
-        //                       origRot:Context_AF.origRot,
-        //                       origScale:Context_AF.origScale
-        //                     };
-        // const avatar = e.detail.cursorEl.parentEl
-        // if (!avatar.classList.contains("locked"))
-        // {
-          Context_AF.el.emit( CIRCLES.EVENTS.SELECT_THIS_OBJECT, this, true );
-        // }
+      Context_AF.el.emit( CIRCLES.EVENTS.SELECT_THIS_OBJECT, this, true );
      });
   },
   update : function(oldData) 
