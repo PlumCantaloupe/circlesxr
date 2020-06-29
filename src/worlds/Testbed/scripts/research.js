298ab35d7301dba216014111bb1299a29811e24b
ascavarelli <anthony.scavarelli@gmail.com>
2020-06-29 12:49:41 -0400

    adding in flexible build functionality into fitts component

--- src/worlds/Testbed/scripts/research.js
+++ src/worlds/Testbed/scripts/research.js
@@ -24,11 +24,19 @@ AFRAME.registerComponent('data-collection', {
 AFRAME.registerComponent('fitts-explore', {
     schema: {},
     init() {
-        //called on 
-        const Context_AF = this;
-        const scene      = document.querySelector('a-scene');
+        const CONTEXT_COMP = this;
 
-        Context_AF.createTargets();
+        CONTEXT_COMP.targetContainer = document.createElement('a-entity');
+        CONTEXT_COMP.targetContainer.setAttribute('id', 'target_container');
+        CONTEXT_COMP.el.appendChild(CONTEXT_COMP.targetContainer);
+
+        CONTEXT_COMP.targetsOuterContainer = document.createElement('a-entity');
+        CONTEXT_COMP.targetsOuterContainer.setAttribute('id', 'targets_outer_container');
+        CONTEXT_COMP.targetContainer.appendChild(CONTEXT_COMP.targetsOuterContainer);
+
+        CONTEXT_COMP.createTargets();
+
+        CONTEXT_COMP.moveTargets(0, 0, 5.0);
     },
     update: function (oldData) {
         //called 
@@ -36,31 +44,45 @@ AFRAME.registerComponent('fitts-explore', {
     tick: function (time, timeDelta) {
     },
     createTargets: function() {
-        const CONTEXT_AF = this;
+        const CONTEXT_COMP = this;
 
         //create fitt's law "spheres"
         const NUM_TARGETS   = 8;
         const ANGLE_BETWEEN = THREE.Math.degToRad(360.0/NUM_TARGETS);
-        const ARC_RADIUS    = 4.0;
-        const TARGET_GEO    = {primitive:'sphere', radius:0.5, segmentsWidth:20, segmentsHeight:20};
-        const TARGET_MAT    = {transparent:false, color:'rgb(57, 187, 130)', emissive:'rgb(7, 137, 80)'};
+        const ARC_RADIUS    = 2.0;
+        const TARGET_GEO    = {primitive:'sphere', radius:0.2, segmentsWidth:20, segmentsHeight:20};
+        const TARGET_MAT    = {transparent:false, color:'rgb(57, 187, 130)', emissive:'rgb(7, 137, 80)', shader:'flat'};
 
-        let pointerVec  = new THREE.Vector3(ARC_RADIUS, 0.0, 0.0);
-        const rotateVec = new THREE.Vector3(0.0, 1.0, 0.0);
-
-        for (let i = 0; i < NUM_TARGETS; i++) {
+        let pointerVec  = new THREE.Vector3(0.0, ARC_RADIUS, 0.0);
+        const rotateVec = new THREE.Vector3(1.0, 0.0, 0.0);
+        const createTarget_f = (x_pos, y_pos, z_pos, unique_id, parentElem) => {
             let target = document.createElement('a-entity');
-            target.setAttribute('id', 'target_' + i);
+            target.setAttribute('id', unique_id);
             target.setAttribute('class', 'interactive fitts_target');
             target.setAttribute('geometry', TARGET_GEO);
             target.setAttribute('material', TARGET_MAT);
-            target.setAttribute('position', {x:pointerVec.x, y:pointerVec.y, z:pointerVec.z});
-            target.setAttribute('circles-interactive-object', {});
-            CONTEXT_AF.el.appendChild(target);
+            target.setAttribute('position', {x:x_pos, y:y_pos, z:z_pos});
+            target.setAttribute('circles-interactive-object', {hovered_scale:1.2, clicked_scale:1.2, neutral_scale:1.0});
+            parentElem.appendChild(target);
+        };
 
-            console.log(pointerVec);
+        //add middle target
+        createTarget_f(0.0, 0.0, 0.0, 'target_selection_0', CONTEXT_COMP.targetContainer);
+
+        //add exterior targets
+        for (let i = 0; i < NUM_TARGETS; i++) {
+            createTarget_f(pointerVec.x, pointerVec.y, pointerVec.z, 'target_selection_' + (i+1), CONTEXT_COMP.targetsOuterContainer);
             pointerVec.applyAxisAngle(rotateVec, ANGLE_BETWEEN);
         }
+    },
+    //x_deg [0, 360], y_deg [0, 360], depth [number]. This will always be set relative to eye/camera position
+    moveTargets : function(x_deg, y_deg, depth) {
+        const CONTEXT_COMP = this;
+
+        let pointerVec  = new THREE.Vector3(depth, 0.0, 0.0); //moving off x-axis as default rotation of cam looks down x
+        const X_VEC     = new THREE.Vector3(1.0, 0.0, 0.0);
+        const Y_VEC     = new THREE.Vector3(0.0, 1.0, 0.0);
+        CONTEXT_COMP.targetContainer.object3D.position.set(5.0, 0.0, 0.0);
     }
 });
 
