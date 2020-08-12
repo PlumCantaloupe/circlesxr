65e63c0340f91ed7da954916d470a4bcb5019912
Anthony Scavarelli <anthony.scavarelli@gmail.com>
2020-08-11 22:32:30 -0400

    adjusting semantics to more clearly define what targets are active and not

--- node_server/modules/research-utils.js
+++ node_server/modules/research-utils.js
@@ -7,29 +7,28 @@ let currTrialIndex          = -1;
 const loadExperimentScript = (data) => {
     // console.log(data);
     // console.log(data.tests.data);
-
-    let type, record, num_targets, targets_x_rots, targets_y_rots, target_widths, target_depths, num_trials = null;
+    let type, record, targets, targets_x_rots, targets_y_rots, target_widths, target_depths, num_trials = null;
     trials = [];
 
     const expArr = data.tests.data;
     for (let i = 0; i < expArr.length; i++) {
         type            = expArr[i].type;
         record          = expArr[i].record;
-        num_targets     = expArr[i].num_targets;
+        targets         = expArr[i].targets;
         targets_x_rots  = expArr[i].targets_x_rots;
         targets_y_rots  = expArr[i].targets_y_rots;
         target_widths   = expArr[i].target_widths;
         target_depths   = expArr[i].target_depths;
         num_trials      = expArr[i].num_trials;
 
-        console.log('type:' + type);
-        console.log('record:' + record);
-        console.log('num_targets:' + num_targets);
-        console.log('targets_x_rots:' + targets_x_rots);
-        console.log('targets_y_rots:' + targets_y_rots);
-        console.log('target_widths:' + target_widths);
-        console.log('target_depths:' + target_depths);
-        console.log('num_trials:' + num_trials);
+        // console.log('type:' + type);
+        // console.log('record:' + record);
+        // console.log('num_targets:' + num_targets);
+        // console.log('targets_x_rots:' + targets_x_rots);
+        // console.log('targets_y_rots:' + targets_y_rots);
+        // console.log('target_widths:' + target_widths);
+        // console.log('target_depths:' + target_depths);
+        // console.log('num_trials:' + num_trials);
 
         total_exp_trials = targets_x_rots.length * targets_y_rots.length * target_widths.length * target_depths.length * num_trials;
         let exp_trials = [];
@@ -56,7 +55,10 @@ const loadExperimentScript = (data) => {
 
                             //create trial
                             let randTrial = {
-                                                activeTarget:   (trial_i + 1) % num_targets,  //may as well loop through all available targets. +1 as '0' is reserved for centre look target
+                                                type:           type,
+                                                record:         record,
+                                                targets:        targets,                            //all targets visible
+                                                target_active:  targets[trial_i % targets.length],  //may as well loop through all available targets. +1 as '0' is reserved for centre look target
                                                 targets_x_rot:  targets_x_rots[xRot_i],
                                                 targets_y_rot:  targets_y_rots[yRot_i],
                                                 target_width:   target_widths[width_i],
@@ -73,6 +75,8 @@ const loadExperimentScript = (data) => {
         //concat with other experiment trials
         trials = trials.concat(exp_trials);
     }
+
+    console.log(trials);
 };
 
 const getNumOfTrialsCompleted = () => {
