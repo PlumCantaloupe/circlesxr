0c63bad737250686542548749aa58410a2622bb5
PlumCantaloupe <anthony.scavarelli@gmail.com>
2021-05-03 00:48:37 -0400

    using sim. click to use so that navmesh still works

--- src/components/circles-sendpoint.js
+++ src/components/circles-sendpoint.js
@@ -2,7 +2,7 @@
 
 AFRAME.registerComponent('circles-sendpoint', {
   schema: {
-    target: {type: 'selector', default:null}    //pass in id to checkpoint you want user to go to ...
+    target: {type:'selector', default:null}    //pass in id to checkpoint (must be checkpoint to work with navmesh) you want user to go to ...
   },
 
   init: function () {
@@ -10,8 +10,7 @@ AFRAME.registerComponent('circles-sendpoint', {
 
     CONTEXT_AF.el.addEventListener('click', (e) => {
       if ( CONTEXT_AF.data.target ) {
-        const checkPos = CONTEXT_AF.data.target.object3D.position;
-        document.querySelector('#Player1').object3D.position.set(checkPos.x, checkPos.y, checkPos.z);
+        const checkPos = CONTEXT_AF.data.target.click();
       }
     });
   },
