c1993915de64acfb889130ff6f7e81f803dd2955
PlumCantaloupe <anthony.scavarelli@gmail.com>
2021-05-11 23:10:27 -0400

    adding in microphone control functionality at startup and during tablet/desktop usage

--- src/components/circles-enter-ui.js
+++ src/components/circles-enter-ui.js
@@ -8,9 +8,36 @@ AFRAME.registerComponent('circles-enter-ui', {
         console.log('scene loaded');
         document.querySelector('#loading-animation-enter').style.display='none';
         document.querySelector('#user-gesture-enter').style.display='block';
+
+        let micOn = false;
+        const toggleMicFunc = (enable) => {
+            micOn = enable;
+            if (micOn) {
+                console.log('enabling microphone');
+                NAF.connection.adapter.enableMicrophone(true);
+                document.querySelector('#button_microphone').style.backgroundImage = "url('/global/images/microphone_on.png')";
+            }
+            else {
+                console.log('disabling microphone');
+                NAF.connection.adapter.enableMicrophone(false);
+                document.querySelector('#button_microphone').style.backgroundImage = "url('/global/images/microphone_off.png')";
+            }
+        }
+
+        //add click listener for starting settings
+        const switch_mic = document.querySelector('#switch_mic');
+        switch_mic.addEventListener('click', function() {
+            toggleMicFunc(switch_mic.checked);
+        });
+
+        //click listener button
+        document.querySelector('#button_microphone').addEventListener('click', function() {
+            toggleMicFunc(!micOn);
+        });
+
         document.querySelector('#user-gesture-enter').addEventListener('click', function() {
-            //hide user-gesture overlay
-            document.querySelector('#user-gesture-overlay').style.display='none';
+            document.querySelector('#user-gesture-overlay').style.display='none';   //hide user-gesture overlay
+            document.querySelector('#ui_wrapper').style.display='block';            //show "extra" controls i.e. microphone toggle
             
             //start all autoplay/ambient music
             const ambientSounds = document.querySelectorAll('.autoplay-sound');
