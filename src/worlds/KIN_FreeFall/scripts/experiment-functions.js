d8621283e3d1b35ab5343bd1d79f488f9751c0c9
Mark McGregor <markmcgregor.contact@gmail.com>
2021-06-01 17:09:33 -0400

    Create physics control panel
    Also refactor scripts and components into separate files

---
+++ src/worlds/KIN_FreeFall/scripts/experiment-functions.js
@@ -0,0 +1,83 @@
+// the list of physics objects that can be used in the scene (and their properties)
+const PHYSICS_OBJECTS = {
+  object1 = {name: "ball", mass: 0.3, assetId=""},
+  object2 = {name: "teddy", mass: 0.1, assetId=""},
+  object3 = {name: "car", mass: 100, assetId=""}
+};
+
+// the gravity control that controls the physics of each object in the scene
+let currentGravityStrength = 1;
+const gravityMaxStrength = 3;
+const gravityMinStrength = 0;
+const gravityIncrementAmount = 0.5;
+
+function startExperiment () {
+  console.log('Starting experiment');
+
+  // get all the free fall objects
+  let freeFallObjects = document.querySelectorAll("[physics-object]");
+
+  // loop through each free fall object
+  freeFallObjects.forEach(element => {
+    // enable physics on the object
+    element.setAttribute('dynamic-body', '');
+  });
+};
+
+function resetExperiment () {
+  console.log('Reseting experiment');
+
+  // get all the free fall objects
+  let freeFallObjects = document.querySelectorAll("[physics-object]");
+
+  // loop through each free fall object
+  freeFallObjects.forEach(element => {
+    // disable physics on the object
+    element.removeAttribute('dynamic-body');
+
+    // reset the position of the object
+    element.emit('resetTransform', {});
+  });
+};
+
+function setLeftObject (object) {
+  console.log('Setting left object to ' + object.name);
+};
+
+function setRightObject (object) {
+  console.log('Setting right object to ' + object.name);
+};
+
+function increaseGravity () {
+  // ensure the gravity is not currently at max value
+  if (currentGravityStrength < gravityMaxStrength) {
+    console.log('Increasing gravity');
+
+    // increase the gravity
+    currentGravityStrength += gravityIncrementAmount;
+
+    // update the gravity strength text
+    gravityStengthText = document.querySelector('#gravityStrengthText');
+    gravityStengthText.setAttribute('text', {
+      value: "Gravity: " + currentGravityStrength,
+      align: "center"
+    });
+  }
+};
+
+function decreaseGravity () {
+  // ensure the gravity is not currently at min value
+  if (currentGravityStrength > gravityMinStrength) {
+    console.log('Decreasing gravity');
+
+    // decrease the gravity
+    currentGravityStrength -= gravityIncrementAmount;
+
+    // update the gravity strength text
+    gravityStengthText = document.querySelector('#gravityStrengthText');
+    gravityStengthText.setAttribute('text', {
+      value: "Gravity: " + currentGravityStrength,
+      align: "center"
+    });
+  }
+}
