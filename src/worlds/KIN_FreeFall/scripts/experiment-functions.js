7d5a5c129882c1fbb4d69f1b51f723a220a42d19
TomasBabkine-DiCaprio <babkinedicapriotomas@gmail.com>
2021-06-15 09:59:00 -0400

    Scaled experiment objects

--- src/worlds/KIN_FreeFall/scripts/experiment-functions.js
+++ src/worlds/KIN_FreeFall/scripts/experiment-functions.js
@@ -1,9 +1,9 @@
 // the list of physics objects that can be used in the scene (and their properties)
 const PHYSICS_OBJECTS = {
-  object1 = {name: "SoccerBall", mass: 0.3, assetId="", height: 4.83, handHeight: 5.55, scale: {x: 0.05, y: 0.05, z: 0.05}, rotationY: 0, mesh: "sphere"},
-  object2 = {name: "Bear", mass: 0.23, assetId="", height: 4.98, handHeight: 5.63, scale: {x: 0.15, y: 0.15, z: 0.15}, rotationY: 180, mesh: "box"},
-  object3 = {name: "Car", mass: 1300, assetId="", height: 3.94, handHeight: 6.01, scale: {x: 1.3, y: 1.3, z: 1.3}, rotationY: 0, mesh: "box"},
-  object4 = {name: "TennisBall", mass: 0.06, assetId="", height: 4.03, handHeight: 4.8, scale: {x: 0.4, y: 0.4, z: 0.4}, rotationY: 0, mesh: "sphere"}
+  object1 = {name: "SoccerBall", mass: 0.3, assetId="", height: 4.83, handHeight: 5.55, mesh: "sphere"},
+  object2 = {name: "Bear", mass: 0.23, assetId="", height: 4.98, handHeight: 5.63, mesh: "box"},
+  object3 = {name: "Car", mass: 1300, assetId="", height: 3.94, handHeight: 6.01, mesh: "box"},
+  object4 = {name: "TennisBall", mass: 0.06, assetId="", height: 4.03, handHeight: 4.8, mesh: "sphere"}
 };
 
 // the gravity control that controls the physics of each object in the scene
