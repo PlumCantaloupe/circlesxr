4989fcbd20789ef245cd08b107e6660b0e03d494
PlumCantaloupe <anthony.scavarelli@gmail.com>
2021-08-23 23:12:28 -0400

    Added in color compoent that changes body parts on click

--- src/components/circles-costume-color-selector.js
+++ src/components/circles-costume-color-selector.js
@@ -1,23 +1,25 @@
 'use strict';
 
 AFRAME.registerComponent('circles-costume-color-selector', {
-    schema: {   
-        colors:         {type: 'array',    default:[  'rgb(250, 228, 200)', 'rgb(243, 212, 167)', 'rgb(230, 193, 151)',
-                                                      'rgb(204, 159, 127)', 'rgb(168, 139, 102)', 'rgb(139, 97, 62)',
-                                                      'rgb(119, 71, 41)',   'rgb(39, 24, 12)',    'rgb(0, 174, 204)',
-                                                      'rgb(57, 202, 137)',  'rgb(85, 78, 255)',   'rgb(255, 85, 68)',
-                                                      'rgb(204, 58, 201)']},   //list of default colors. Best for skin colours ... 
-        class_selector: {type: 'string',    default:''}   //this is the class we will use to query for and set the color here
+    schema: {
+      colors: {
+        default: 'rgb(250, 228, 200),rgb(243, 212, 167),rgb(230, 193, 151), rgb(204, 159, 127),rgb(168, 139, 102),rgb(139, 97, 62), rgb(119, 71, 41), rgb(39, 24, 12),rgb(0, 174, 204), rgb(57, 202, 137), rgb(85, 78, 255), rgb(255, 85, 68), rgb(204, 58, 201)',
+        parse: function (value) {
+          const newVal = value.replace(/\s|\\/g, ''); //remove spaces and any backslashes added by Aframe
+          return newVal.split(/,\s*(?![^()]*\))/);    //regex to split by commas outside of brackets
+        },
+        stringify: function (value) {
+          return value.join('/');
+        }
+      },
+      class_selector: {type: 'string',    default:''}   //this is the class we will use to query for and set the color here
     },
     init: function() {
       const CONTEXT_AF = this;
       const data = CONTEXT_AF.data;
 
-      console.log(data.colors);
-      console.log(data.class_selector);
-
       const click_func = (e) => {
-        const col = e.detail.el.components['circles-color'].data.color;
+        const col = e.target.components['circles-color'].data.color;
         const colTargets = document.querySelectorAll(CONTEXT_AF.data.class_selector);
 
         colTargets.forEach((elem) => {
@@ -33,7 +35,7 @@ AFRAME.registerComponent('circles-costume-color-selector', {
         colElem.setAttribute('geometry', {primitive:'box', width:0.1, height:0.2, depth:0.05});
         colElem.setAttribute('circles-color', {color:data.colors[i]});
         colElem.setAttribute('circles-interactive-object', '');
-        colElem.addEventListener('event', click_func);
+        colElem.addEventListener('click', click_func);
         CONTEXT_AF.el.append(colElem);
 
         x_step += 0.15;
