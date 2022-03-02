441f19971de1556716f25f1e83c27b7fe2166c6f
judyc <jchang.ty@gmail.com>
2022-03-02 12:55:01 -0500

    Removed highres files, rename lowres to MemoryPalace and WIP branch

--- src/worlds/COG_MemoryPalace_New/components/easy-transparency.js
+++
@@ -1,8 +0,0 @@
-AFRAME.registerComponent('easy-transparency', {
-  init: function() {
-    const self = this;
-    const el = this.el;
-
-    AFRAME.utils.entity.setComponentProperty(el, 'material.alphaTest', 0.2);
-  }
-});
