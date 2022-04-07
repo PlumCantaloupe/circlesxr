8b6f1c2f25bd43dc5b3898d2fb8d6c80af039d24
pekflyer <yongxiaofeng@gmail.com>
2022-04-06 22:06:45 -0400

    finishing up the adjustment for chairs and labels

--- src/worlds/Meeting_Place/assets/scripts/main.js
+++ src/worlds/Meeting_Place/assets/scripts/main.js
@@ -145,40 +145,141 @@ AFRAME.registerComponent('chair-maker', {
         ry = 160;
         px = 3.0;
         pz = 3.0;
-        while(x < 4)  {
+        while(x < 1)  {
             console.log(x);
-            while(y<11)
-            {   
+            //while(y<10)
+            //{   
                 console.log('Starting x: '+px);
                 console.log('Starting z: '+pz);
                 CONTEXT_AF.newchair = document.createElement('a-entity');
                 CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
                 CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
-                CONTEXT_AF.newchair.setAttribute('position',{x:-(px), y:0, z:-(pz)});
+                CONTEXT_AF.newchair.setAttribute('position',{x:-0.314, y:0, z:-8.330});
                 CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
-                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:-(ry), z:0});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:178.947, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:5.046, y:0, z:-6.897});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:140.306, z:0});
                 scene.appendChild(CONTEXT_AF.newchair);
                 y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:-5.465, y:0, z:-6.362});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:-145.016, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:-6.383, y:0, z:-17.342});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:-164.220, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:13.155, y:0, z:-13.543});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:134.123, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:-4.387, y:0, z:-12.382});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:-159.990, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:4.118, y:0, z:-12.189});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:164.059, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:-10.293, y:0, z:-7.786});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:-150.459, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:9.836, y:0, z:-7.801});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:122.744, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:-12.570, y:0, z:-13.562});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:-144.211, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;  
+                console.log('Starting x: '+px);
+                console.log('Starting z: '+pz);
+                CONTEXT_AF.newchair = document.createElement('a-entity');
+                CONTEXT_AF.newchair.setAttribute('id','chair0'+x+y);
+                CONTEXT_AF.newchair.setAttribute('gltf-model', '/worlds/Meeting_Place/assets/models/objects/meetingobjects/meeting-chair/meeting-chair.glb');
+                CONTEXT_AF.newchair.setAttribute('position',{x:4.044, y:0, z:-17.967});
+                CONTEXT_AF.newchair.setAttribute('scale',{x:0.4, y:0.4, z:0.4});
+                CONTEXT_AF.newchair.setAttribute('rotation',{x:0, y:167.769, z:0});
+                scene.appendChild(CONTEXT_AF.newchair);
+                y++;    
+
                 //ry = ry+ 20; 
-                px = px + 1.898; 
-                pz = pz + 1.16;
-                console.log('ending x: '+ px);
-                console.log('ending z: '+ pz);
-            }
-            ry = 160;
-            px = px-2;
-            if(x == 1 || x == 2)
-            {
-                if(x==1){
-                    pz = -4;
-                }
-                else{
-                    pz= 4;
-                }
+                //px = px + 1.898; 
+                //pz = pz + 1.16;
+                //console.log('ending x: '+ px);
+                //console.log('ending z: '+ pz);
+            //}
+            // ry = 160;
+            // px = px-2;
+            // if(x == 1 || x == 2)
+            // {
+            //     if(x==1){
+            //         pz = -4;
+            //     }
+            //     else{
+            //         pz= 4;
+            //     }
                 
-            }
-            px = 3.0;
-            x++;
+            // }
+            // px = 3.0;
+             x++;
         }
     },
 });
