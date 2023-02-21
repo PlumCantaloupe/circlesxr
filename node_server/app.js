b639ff30ec5cf62fe3696c0a214ecf3919d06c8c
PlumCantaloupe <anthony.scavarelli@gmail.com>
2023-02-21 12:48:40 -0500

    adding tests

--- node_server/app.js
+++ node_server/app.js
@@ -270,7 +270,7 @@ io.on("connection", socket => {
 ///
 
 //trying to create a system for easy communication here.
-io.on("connection", socket => {
+io.on("connection", (socket) => {
   console.log('connection test for general circles messaging system');
 
   //to catch all events: https://stackoverflow.com/questions/10405070/socket-io-client-respond-to-all-events-with-one-handler
