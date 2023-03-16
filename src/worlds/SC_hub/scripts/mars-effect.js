1516220042235dcc4cf7606be948e0c63d5cfc9d
NanosHere <71567368+NanosHere@users.noreply.github.com>
2023-03-16 13:31:33 -0400

    added assets from a2

---
+++ src/worlds/SC_hub/scripts/mars-effect.js
@@ -0,0 +1,59 @@
+'use strict';
+
+//this component will basically just toggle off/on the spinning of the walls
+AFRAME.registerComponent('mars-effect', {
+  schema: {
+    duration: {type: 'number', default:20000.0},  //duration is in milliseconds
+  },
+  multiple: false, //do not allow multiple instances of this component on this entity
+  init: function() {
+    
+    //get a local reference to our entities and set some property variables
+    const Context_AF = this;
+    
+    Context_AF.marsLocator      = document.querySelector('#universal_MarsLocator');
+    Context_AF.mars      = document.querySelector('#mars');
+    Context_AF.box      = document.querySelector('#box-mars');
+    Context_AF.isSpinning = false;
+    
+    //let's add the basic animation to teh walls entity
+    //note that it is not enabled initially
+    
+    Context_AF.mars.setAttribute('animation', {property:'rotation.y', to:360, loop:true, dur:Context_AF.data.duration, easing:'linear', enabled:false});
+    Context_AF.marsLocator.setAttribute('animation', {property:'rotation.y', to:360, loop:true, dur:Context_AF.data.duration, easing:'linear', enabled:false});
+    
+    //listen on click
+    Context_AF.el.addEventListener('click', function() {
+      if (Context_AF.isSpinning == true) {
+        Context_AF.mars.setAttribute("visible",false);
+        console.log('stop spinning');
+        console.log('hide mars')
+        Context_AF.mars.setAttribute('animation', {enabled:false});
+        //Context_AF.marsLocator.setAttribute('animation', {enabled:false});
+        Context_AF.isSpinning = false;
+        Context_AF.box.setAttribute('material', "color:rgb(255, 255, 255)");
+      }
+      else {
+        console.log('spinning');
+        
+        console.log('show mars')
+        Context_AF.mars.setAttribute('animation', {enabled:true});
+        Context_AF.marsLocator.setAttribute('animation', {enabled:true});
+        Context_AF.isSpinning = true;
+        Context_AF.mars.setAttribute("visible",true);
+        Context_AF.box.setAttribute('material', "color:rgb(0, 255, 0)");
+      }
+    });
+    
+  },
+  
+  //component documentation: https://github.com/aframevr/aframe/blob/master/docs/core/component.md
+  
+  // update: function (oldData) {},
+  // tick: function(time, timeDelta) {},
+  // tock: function(time, timeDelta) {},
+  // remove: function() {},
+  // pause: function() {},
+  // play: function() {},
+  // updateScheme: function(data) {}
+});
---
+++ src/worlds/SC_hub/scripts/uranus-effect.js
@@ -0,0 +1,60 @@
+'use strict';
+
+//this component will basically just toggle off/on the spinning of the walls
+AFRAME.registerComponent('uranus-effect', {
+  schema: {
+    duration: {type: 'number', default:20000.0},  //duration is in milliseconds
+  },
+  multiple: false, //do not allow multiple instances of this component on this entity
+  init: function() {
+    
+    //get a local reference to our entities and set some property variables
+    const Context_AF = this;
+    
+    Context_AF.uranusLocator      = document.querySelector('#universal_UranusLocator');
+    Context_AF.uranus      = document.querySelector('#uranus');
+    Context_AF.box      = document.querySelector('#box-uranus');
+    Context_AF.isSpinning = false;
+    
+    //let's add the basic animation to teh walls entity
+    //note that it is not enabled initially
+    
+    Context_AF.uranus.setAttribute('animation', {property:'rotation.y', to:360, loop:true, dur:Context_AF.data.duration, easing:'linear', enabled:false});
+    Context_AF.uranusLocator.setAttribute('animation', {property:'rotation.y', to:360, loop:true, dur:Context_AF.data.duration, easing:'linear', enabled:false});
+    
+    //listen on click
+    Context_AF.el.addEventListener('click', function() {
+      if (Context_AF.isSpinning === true) {
+        Context_AF.uranus.setAttribute("visible",false);
+        console.log('stop spinning');
+        console.log('hide mars')
+        Context_AF.uranus.setAttribute('animation', {enabled:false});
+        Context_AF.uranusLocator.setAttribute('animation', {enabled:false});
+        Context_AF.isSpinning = false;
+        Context_AF.box.setAttribute('material', "color:rgb(255, 255, 255)");
+      }
+      else {
+        console.log('spinning');
+        
+        console.log('show mars')
+        Context_AF.uranus.setAttribute('animation', {enabled:true});
+        Context_AF.uranusLocator.setAttribute('animation', {enabled:true});
+        Context_AF.isSpinning = true;
+        Context_AF.uranus.setAttribute("visible",true);
+        Context_AF.box.setAttribute('material', "color:rgb(0, 255, 0)");
+        
+      }
+    });
+    
+  },
+  
+  //component documentation: https://github.com/aframevr/aframe/blob/master/docs/core/component.md
+  
+  // update: function (oldData) {},
+  // tick: function(time, timeDelta) {},
+  // tock: function(time, timeDelta) {},
+  // remove: function() {},
+  // pause: function() {},
+  // play: function() {},
+  // updateScheme: function(data) {}
+});
