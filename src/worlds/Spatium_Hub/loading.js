a98317204ee416e1f4fc035005fcfbf7ca8fb764
nisaa1331 <anisajohnny31@gmail.com>
2023-03-21 18:09:03 -0400

    started the start screen

---
+++ src/worlds/Spatium_Hub/loading.js
@@ -0,0 +1,6 @@
+// adds a loading screen before the user accesses the hub
+
+document.querySelector("button").onclick = function() {
+    document.querySelector("#loading").style.display = "none"
+  }
+  
