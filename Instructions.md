832f40df67932efcc1f8dbd69596276008343085
v-claire <81376520+v-claire@users.noreply.github.com>
2024-02-02 15:37:42 -0500

    Powershell startup script created

---
+++ Instructions.md
@@ -0,0 +1,4 @@
+To set up the application, open a terminal and run the following script:
+
+> start.ps1
+
---
+++ start.ps1
@@ -0,0 +1,7 @@
+npm install
+npm install socket.io --save
+npm install express --save
+npm init -y
+cp .env.dist .env
+
+node ./app.js
