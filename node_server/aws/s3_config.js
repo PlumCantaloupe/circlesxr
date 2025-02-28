ae093af2b0c88566a4829f2bc22470f67a4a4f6f
aaron-chase <a82chaser@gmail.com>
2025-02-28 10:59:51 -0500

    Initial aws config

--- node_server/aws/s3_config.js
+++ node_server/aws/s3_config.js
@@ -1,9 +1,10 @@
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
 // For Node version >= 14, `require()` is deprecated
 // The following lines enable the use of the `require()` function,
 // as used throughout the rest of CirclesXR
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
 
+console.log('TEST TEST TEST TEST');
 // retrieve environment variables
 const dotenv = require('dotenv');
 const dotenvParseVariables = require('dotenv-parse-variables');
@@ -18,14 +19,33 @@ if (env.AWS_ACCESS_KEY == undefined || env.AWS_REGION == undefined || env.AWS_SE
 
 // configure aws s3 client using env variables
 const aws = require('@aws-sdk/client-s3');
-const s3 = new aws.S3Client({
+module.exports = new aws.S3Client({
     "region": env.AWS_REGION,
     "accessKeyId": env.AWS_ACCESS_KEY,
     "secretKeyAccess": env.AWS_SECRET_TOKEN,
 });
 
-// test to retrieve the s3 bucket
-// should return the "circlesxr_objectbucket" bucket
-const command = new aws.ListBucketsCommand();
-const response = await s3.send(command);
-console.log(response);
+// const bucketName = 'circlesxr-objects';
+
+// // test to retrieve the s3 bucket
+// // should return the "circlesxr_objectbucket" bucket
+// const command = new aws.ListBucketsCommand();
+// const bucketResponse = await s3.send(command);
+// console.log('// --- LOAD BUCKETS --- //');
+// console.log(bucketResponse);
+
+// const objectKey = "OWL_OBJECT";
+
+// const fs = require('fs').promises;
+// const owlFile = await fs.readFile('./node_server/aws/owl_object.glb');
+
+// // add an object into the bucket
+// const addObjectCmd = new aws.PutObjectCommand({
+//     Bucket: bucketName,
+//     Key: objectKey,
+//     Body: owlFile.toString()
+// });
+// const addResponse = await s3.send(addObjectCmd);
+// console.log('\n// --- ADDING OBJECT --- //');
+// console.log(addResponse);
+
