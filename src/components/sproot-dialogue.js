9e900a833b2949972f032501f592075a7947c24c
BellaMacc <meimei.macculloch@gmail.com>
2025-03-14 02:25:47 -0400

    stuff

--- src/components/sproot-dialogue.js
+++ src/components/sproot-dialogue.js
@@ -6,32 +6,54 @@ AFRAME.registerComponent('sproot-dialogue', {
     },
     init: function() {
       const CONTEXT_AF = this;
-      console.log("i am registered!");
-      document.querySelector("#sproot_collider").addEventListener('click', function(e) {
-        console.log("I am listening!");
-        CONTEXT_AF.playDialogue();
+     
+      document.addEventListener(CIRCLES.EVENTS.READY, function() {
+        
+        console.log("i am registered!");
+        
+        //if (CONTEXT_AF.el.hasAttribute('circles-interactive-object') === false) {
+         //  CONTEXT_AF.el.setAttribute('circles-interactive-object', {});
+       // } 
+       CONTEXT_AF.el.addEventListener('click', CONTEXT_AF.playDialogue());
+  
       });
+      
+      //make sure this is interactive
+    
+ 
     },
     update : function(oldData) {
      
     },
     tick : function (time, timeDelta) {
-      
+    
     },
     playDialogue : function() {
+      
+
       const CONTEXT_AF = this;
       console.log("I am clicked!");
       if (!CONTEXT_AF.isPlaying){
-        console.log("I am clicked!");
+        console.log("I am playing!");
         CONTEXT_AF.isPlaying = true;
         let sproot = document.querySelector("#sproot");
-        let dialogue = DocumentTimeline.querySelector("#dialogue");
-        sproot.setAttribute("animation-mixer", "clip: Sproot_Waving");
-        dialogue.components['sound'].play();
-
-
+        let dialogue = document.querySelector("#dialogue");
+        sproot.setAttribute("animation-mixer", "clip: Sproot_Waving; loop:once");
+        dialogue.setAttribute('circles-sound', {state: 'play'});
+        CONTEXT_AF.el.addEventListener('animation-finished', endFunction());
+        
+        function endFunction(){
+          CONTEXT_AF.el.removeEventListener('animation-finished', endFunction());
+          sproot.setAttribute("animation-mixer", "clip: Sproot_Idle; loop:repeat");
+          CONTEXT_AF.isPlaying = false;
+        }
 
       }
-    }
+    },
+    prepElemsForInteraction: function(){
+      console.log("I am active");
+      
+    },
+    
     
   });
