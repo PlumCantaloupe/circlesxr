5f290b381c1d9afdfb8bedf454a61a14162df50a
Msganaot <msgana27@hotmail.com>
2025-03-19 16:07:47 -0400

    fixed mobile cave painting interaction

--- src/worlds/LitPortals-Cave/canvas.js
+++ src/worlds/LitPortals-Cave/canvas.js
@@ -4,11 +4,12 @@ const container = document.getElementById("canvasDiv")
 
 let halfCanvas = canvas.width/2;
 
-const ongoingTouches = [];
 
 
 context.fillStyle = "#964B00";
 context.fillRect(0,0, canvas.width, canvas.height);
+
+
 
 let drawColour = "black";
 let drawSize = "3";
@@ -16,9 +17,9 @@ let drawSize = "3";
 let isDrawing = false;
 
 //event listeners for when mouse or touch input occurs
-canvas.addEventListener("touchstart", startTouch);
-canvas.addEventListener("touchmove", drawTouch);
-canvas.addEventListener("touchend", stopTouch);
+canvas.addEventListener("touchstart", startTouch, false);
+canvas.addEventListener("touchmove", drawTouch, false);
+canvas.addEventListener("touchend", stopTouch, false);
 canvas.addEventListener("mousedown", start, false);
 canvas.addEventListener("mousemove", draw, false);
 canvas.addEventListener("mouseup", stop, false);
@@ -32,6 +33,11 @@ function start(event){
     context.beginPath();
     context.moveTo(event.clientX - canvas.offsetLeft + halfCanvas, event.clientY - canvas.offsetTop + halfCanvas);
     // context.moveTo(event.offsetX, event.offsetY);
+
+  let x = event.clientX - canvas.offsetLeft + halfCanvas;
+  let y = event.clientY - canvas.offsetTop + halfCanvas;
+  let text = "Click X coords: " + x + ", Click Y coords: " + y;
+  document.getElementById("clickCoords").innerHTML = text;
 
     event.preventDefault();
 }
@@ -64,6 +70,10 @@ function draw(event){
         console.log("x: " + event.offsetX);
         console.log("y: " + event.offsetY);
         context.stroke();
+        let x = event.clientX - canvas.offsetLeft + halfCanvas;
+  let y = event.clientY - canvas.offsetTop + halfCanvas;
+  let text = "click X coords: " + x + ", Click Y coords: " + y;
+  document.getElementById("clickCoords").innerHTML = text;
     }
     event.preventDefault();
 
@@ -79,130 +89,51 @@ function stop (event){
 }
 
 // https://developer.mozilla.org/en-US/docs/Web/API/Touch_events
+// on touchstart event
 function startTouch(event){
     isDrawing = true;
-    // context.beginPath();
-    // context.moveTo(event.clientX - canvas.offsetLeft + halfCanvas, event.clientY - canvas.offsetTop + halfCanvas);
-    context.fillStyle = "#red";
-    context.fillRect(0,0, canvas.width, canvas.height);
+    context.beginPath();
+    context.moveTo( event.touches[0].clientX - canvas.offsetLeft + halfCanvas, event.touches[0].clientY - canvas.offsetTop + halfCanvas);
+    // context.fillStyle = "red";
+    // context.fillRect(50,50, 50, 50);
 
-    const touches = event.changedTouches;
+    let x = event.touches[0].clientX;
+    let y = event.touches[0].clientY;
+    let text = "Touch X coords: " + x + ", Touch Y coords: " + y;
+    document.getElementById("touchCoords").innerHTML = text;
 
-    for (let i = 0; i < touches.length; i++) {
-        log(`touchstart: ${i}.`);
-        ongoingTouches.push(copyTouch(touches[i]));
-        const color = colorForTouch(touches[i]);
-        log(`color of touch with id ${touches[i].identifier} = ${color}`);
-        context.beginPath();
-        context.arc(touches[i].pageX, touches[i].pageY, 4, 0, 2 * Math.PI, false); // a circle at the start
-        context.fillStyle = color;
-        context.fill();
-      }
-
-      event.preventDefault();
-
+    event.preventDefault();
 
 }
-
+// on touchmove event
 function drawTouch(event){
 
-    const touches = event.changedTouches;
-
-    // if(isDrawing){
-    //     context.lineTo(event.clientX - canvas.offsetLeft + halfCanvas, event.clientY - canvas.offsetTop + halfCanvas);
-    //     context.strokeStyle = drawColour;
-    //     context.lineWidth = drawSize;
-    //     context.stroke();
-
-   
-    // }
-
-    for (let i = 0; i < touches.length; i++) {
-        const color = colorForTouch(touches[i]);
-        const idx = ongoingTouchIndexById(touches[i].identifier);
-    
-        if (idx >= 0) {
-          log(`continuing touch ${idx}`);
-          context.beginPath();
-          log(
-            `ctx.moveTo( ${ongoingTouches[idx].pageX}, ${ongoingTouches[idx].pageY} );`,
-          );
-          context.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-          log(`ctx.lineTo( ${touches[i].pageX}, ${touches[i].pageY} );`);
-          context.lineTo(touches[i].pageX, touches[i].pageY);
-          context.lineWidth = 4;
-          context.strokeStyle = color;
-          context.stroke();
-    
-          ongoingTouches.splice(idx, 1, copyTouch(touches[i])); // swap in the new touch record
-        } else {
-          log("can't figure out which touch to continue");
-        }
-      }
-    event.preventDefault();
-
-}
-
-function stopTouch (event){
-    // if(isDrawing){
-    //     context.stroke();
-    //     context.closePath();
-    //     isDrawing = false; 
-    // }
-    
-
-    event.preventDefault();
-
-    log("touchend");
-    const touches = event.changedTouches;
-
-    for (let i = 0; i < touches.length; i++) {
-        const color = colorForTouch(touches[i]);
-        let idx = ongoingTouchIndexById(touches[i].identifier);
-    
-        if (idx >= 0) {
-          context.lineWidth = 4;
-          context.fillStyle = color;
-          context.beginPath();
-          context.moveTo(ongoingTouches[idx].pageX, ongoingTouches[idx].pageY);
-          context.lineTo(touches[i].pageX, touches[i].pageY);
-          context.fillRect(touches[i].pageX - 4, touches[i].pageY - 4, 8, 8); // and a square at the end
-          ongoingTouches.splice(idx, 1); // remove it; we're done
-        } else {
-          log("can't figure out which touch to end");
-        }
-      }
-
-
-}
-
-function colorForTouch(touch) {
-    let r = touch.identifier % 16;
-    let g = Math.floor(touch.identifier / 3) % 16;
-    let b = Math.floor(touch.identifier / 7) % 16;
-    r = r.toString(16); // make it a hex digit
-    g = g.toString(16); // make it a hex digit
-    b = b.toString(16); // make it a hex digit
-    const color = `#${r}${g}${b}`;
-    return color;
-  }
-
-  function copyTouch({ identifier, pageX, pageY }) {
-    return { identifier, pageX, pageY };
-  }
-
-  function ongoingTouchIndexById(idToFind) {
-    for (let i = 0; i < ongoingTouches.length; i++) {
-      const id = ongoingTouches[i].identifier;
-  
-      if (id === idToFind) {
-        return i;
-      }
+    if(isDrawing){
+        context.lineTo(event.touches[0].clientX - canvas.offsetLeft + halfCanvas, event.touches[0].clientY - canvas.offsetTop + halfCanvas);
+        context.strokeStyle = drawColour;
+        context.lineWidth = drawSize;
+        context.stroke();
     }
-    return -1; // not found
-  }
+    // context.fillStyle = "blue";
+    // context.fillRect(50,50, 50, 50);
+    let x = event.touches[0].clientX - canvas.offsetLeft + halfCanvas;
+    let y = event.touches[0].clientY - canvas.offsetTop + halfCanvas;
+    let text = "Touch X coords: " + x + ", Touch Y coords: " + y;
+    document.getElementById("touchCoords").innerHTML = text;
 
-  function log(msg) {
-    const container = document.getElementById("log");
-    container.textContent = `${msg} \n${container.textContent}`;
-  }
+    event.preventDefault();
+}
+
+// on touchend event
+function stopTouch (event){
+    if(isDrawing){
+        context.stroke();
+        context.closePath();
+        isDrawing = false; 
+    }
+    // context.fillStyle = "green";
+    // context.fillRect(50,50, 50, 50);
+    
+    event.preventDefault();
+}
+
