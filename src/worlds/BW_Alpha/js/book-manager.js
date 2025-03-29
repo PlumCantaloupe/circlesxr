d1cce50c40a195df841bcb079d55c21cf37ec5ed
kayla <kaylajzhang@gmail.com>
2025-03-29 02:09:54 -0400

    day  2: music and particle go brrrr

--- src/worlds/BW_Alpha/js/book-manager.js
+++ src/worlds/BW_Alpha/js/book-manager.js
@@ -36,6 +36,31 @@ AFRAME.registerComponent('book-manager', {
         CONTEXT_AF.book4 = scene.querySelector('#book4');
         CONTEXT_AF.testbtn = scene.querySelector('#tempbutton');
 
+
+        CONTEXT_AF.track1 = scene.querySelector('#track1');
+        CONTEXT_AF.position1 = {x: 0, y: 1.213, z: -12.319};
+        CONTEXT_AF.rotation1 = {x: 0, y: 0, z: 0};
+        CONTEXT_AF.book1c1 = {x: -0.397, y: 1.472, z: -12.603};
+        CONTEXT_AF.book1c2 = {x: 0.392, y: 1.108, z: -12.079};
+
+        CONTEXT_AF.track2 = scene.querySelector('#track2');
+        CONTEXT_AF.position2 = {x: 1.639, y: 1.213, z: -13.925};
+        CONTEXT_AF.rotation2 = {x: 0, y: 90, z: 0};
+        CONTEXT_AF.book2c1 = {x: 1.367, y: 1.472, z: -13.528};
+        CONTEXT_AF.book2c2 = {x: 1.847, y: 1.108, z: -14.317};
+
+        CONTEXT_AF.track3 = scene.querySelector('#track3');
+        CONTEXT_AF.position3 = {x: 0, y: 1.213, z: -15.53};
+        CONTEXT_AF.rotation3 = {x: 0, y: 180, z: 0};
+        CONTEXT_AF.book3c1 = {x: 0.397, y: 1.472, z: -15.247};
+        CONTEXT_AF.book3c2 = {x: -0.392, y: 1.108, z: -15.733};
+
+        CONTEXT_AF.track4 = scene.querySelector('#track4');
+        CONTEXT_AF.position4 = {x: -1.639, y: 1.213, z: -13.925};
+        CONTEXT_AF.rotation4 = {x: 0, y: 270, z: 0};
+        CONTEXT_AF.book4c1 = {x: -1.367, y: 1.472, z: -14.317};
+        CONTEXT_AF.book4c2 = {x: -1.847, y: 1.108, z: -13.528};
+
         CONTEXT_AF.location1 = 0;
         CONTEXT_AF.location2 = 0;
         CONTEXT_AF.location3 = 0;
@@ -61,6 +86,51 @@ AFRAME.registerComponent('book-manager', {
         CONTEXT_AF.testbtn.addEventListener('click', function(){
             console.log("set book locations");
             CONTEXT_AF.setLocations();
+        });
+
+        CONTEXT_AF.book1.addEventListener(CIRCLES.EVENTS.RELEASE_THIS_OBJECT, () => {
+            console.log(CONTEXT_AF.book1.getAttribute('position'));
+            if(CONTEXT_AF.betweenPos(CONTEXT_AF.book1.getAttribute('position'), CONTEXT_AF.book1c1, CONTEXT_AF.book1c2)){
+                //emit to others
+                //dont need to emit position or rotation i think idk (only host can place book rn idk why)
+                CONTEXT_AF.book1.setAttribute('position', CONTEXT_AF.position1);
+                CONTEXT_AF.book1.setAttribute('rotation', CONTEXT_AF.rotation1);
+                CONTEXT_AF.book1.setAttribute('gltf-model', '#openbook_model');
+                CONTEXT_AF.startMusic(1);
+            }
+        });
+
+        CONTEXT_AF.book2.addEventListener(CIRCLES.EVENTS.RELEASE_THIS_OBJECT, () => {
+            console.log(CONTEXT_AF.book1.getAttribute('position'));
+            if(CONTEXT_AF.betweenPos(CONTEXT_AF.book2.getAttribute('position'), CONTEXT_AF.book2c1, CONTEXT_AF.book2c2)){
+                //emit to others
+                CONTEXT_AF.book2.setAttribute('position', CONTEXT_AF.position2);
+                CONTEXT_AF.book2.setAttribute('rotation', CONTEXT_AF.rotation2);
+                CONTEXT_AF.book2.setAttribute('gltf-model', '#openbook_model');
+                CONTEXT_AF.startMusic(2);
+            }
+        });
+
+        CONTEXT_AF.book3.addEventListener(CIRCLES.EVENTS.RELEASE_THIS_OBJECT, () => {
+            console.log(CONTEXT_AF.book3.getAttribute('position'));
+            if(CONTEXT_AF.betweenPos(CONTEXT_AF.book3.getAttribute('position'), CONTEXT_AF.book3c1, CONTEXT_AF.book3c2)){
+                //emit to others
+                CONTEXT_AF.book3.setAttribute('position', CONTEXT_AF.position3);
+                CONTEXT_AF.book3.setAttribute('rotation', CONTEXT_AF.rotation3);
+                CONTEXT_AF.book3.setAttribute('gltf-model', '#openbook_model');
+                CONTEXT_AF.startMusic(3);
+            }
+        });
+
+        CONTEXT_AF.book4.addEventListener(CIRCLES.EVENTS.RELEASE_THIS_OBJECT, () => {
+            console.log(CONTEXT_AF.book4.getAttribute('position'));
+            if(CONTEXT_AF.betweenPos(CONTEXT_AF.book4.getAttribute('position'), CONTEXT_AF.book4c1, CONTEXT_AF.book4c2)){
+                //emit to others
+                CONTEXT_AF.book4.setAttribute('position', CONTEXT_AF.position4);
+                CONTEXT_AF.book4.setAttribute('rotation', CONTEXT_AF.rotation4);
+                CONTEXT_AF.book4.setAttribute('gltf-model', '#openbook_model');
+                CONTEXT_AF.startMusic(4);
+            }
         });
 
         if (CIRCLES.isCirclesWebsocketReady()) {
@@ -99,7 +169,6 @@ AFRAME.registerComponent('book-manager', {
     },
 
     randNum: function (quantity, min, max) {
-        const CONTEXT_AF = this;
         //randomize quantity amount of unique numbers betwee min and max
         const numbers = []
             while(numbers.length < quantity){
@@ -109,5 +178,26 @@ AFRAME.registerComponent('book-manager', {
                 }
             }
         return(numbers);
-    }
+    },
+
+    betweenPos: function (position, c1, c2){
+        //assign the coordinates of the corners to min and max values
+        const minX = Math.min(c1.x, c2.x);
+        const maxX = Math.max(c1.x, c2.x);
+        const minY = Math.min(c1.y, c2.y);
+        const maxY = Math.max(c1.y, c2.y);
+        const minZ = Math.min(c1.z, c2.z);
+        const maxZ = Math.max(c1.z, c2.z);
+
+        return (position.x >= minX && position.x <= maxX && 
+            position.y >= minY && position.y <= maxY &&
+            position.z >= minZ && position.z <= maxZ);
+    },
+
+    // set locations of books
+    startMusic: function (book) {
+        const CONTEXT_AF = this;
+        
+        CONTEXT_AF[`track${book}`].setAttribute('circles-sound', `src:#track${book}-music; autoplay: true; loop: true; type: music; poolsize:1; volume: 1`);
+    },
 });
