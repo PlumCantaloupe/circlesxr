ae144f0e941cb3d40abf6d442a915ac8367dc509
Emma Souannhaphanh <77692774+etretf@users.noreply.github.com>
2025-04-11 21:13:38 -0400

    Remove schema attr and update comment

--- src/worlds/BW_AudioDemo/js/scale-visualiser.js
+++ src/worlds/BW_AudioDemo/js/scale-visualiser.js
@@ -2,12 +2,11 @@ AFRAME.registerComponent('scale-visualiser', {
     dependencies: ['audioanalyser'],
     schema: {
         modifier: {type: 'number', default: 1},
-        minSize: {type: 'number', default: 1},
         maxSize: {type: 'number', default: 5}
     },
     tick: function () {
         const vol = this.el.components.audioanalyser.volume;
-        // Get volume from the audioanalyser component and adjust the volume according to the min size, max size, and modifier
+        // Get volume from the audioanalyser component and adjust the volume
         const adjVol = Math.min(vol * this.data.modifier, this.data.maxSize);
 
         // Scale the object according to the adjust volume
