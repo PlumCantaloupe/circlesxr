7cddc0fa27d7662709eef3fa3fb425b12ccc89ef
acce8711 <101413675+acce8711@users.noreply.github.com>
2025-04-11 22:57:40 -0400

    gudiing tetx bug

--- src/worlds/BW_Hub/js/set-spawnpoint.js
+++ src/worlds/BW_Hub/js/set-spawnpoint.js
@@ -5,7 +5,6 @@ AFRAME.registerComponent('spawn-point', {
         CONTEXT_AF.sharedStateManager = document.querySelector('[bw-shared-state-manager]').components['bw-shared-state-manager'];
         CONTEXT_AF.room = CONTEXT_AF.sharedStateManager.getData(BRAINWAVES.LS_RECENT_ROOM);
         CONTEXT_AF.roomShortName = CONTEXT_AF.sharedStateManager.getRoom();
-        CONTEXT_AF.guidingText = document.querySelector('[bw-guiding-text]').components['bw-guiding-text'];
 
         CONTEXT_AF.scene = CONTEXT_AF.el.sceneEl;
 
@@ -37,6 +36,7 @@ AFRAME.registerComponent('spawn-point', {
         //when ready rotate the player so they face the control panel
         CONTEXT_AF.scene.addEventListener(CIRCLES.EVENTS.READY, function () {
             CONTEXT_AF.avatarRig = CIRCLES.getAvatarRigElement();
+            CONTEXT_AF.guidingText = document.querySelector('[bw-guiding-text]').components['bw-guiding-text'];
 
             if(CONTEXT_AF.room === 'BW_Hub') {
                 CONTEXT_AF.avatarRig.object3D.rotation.y = THREE.MathUtils.degToRad(277);
